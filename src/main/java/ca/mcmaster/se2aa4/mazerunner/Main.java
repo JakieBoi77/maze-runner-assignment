69be1d6e65a538b447b4ec3bfd095a2eeab1f1be
JakieBoi77 <jakef7@icloud.com>
2024-01-17 17:28:05 -0500

    Enabled the maze file to be passed in thorugh the -i flag using Apache CLI

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -1,10 +1,14 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class Main {
 
@@ -13,8 +17,22 @@ public class Main {
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
         try {
-            logger.info("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            //Uses Apache CLI to parse -i flag
+            Options options = new Options();
+            options.addOption("-i", true, "The path to the maze file.");
+            CommandLineParser parser = new DefaultParser();
+            String maze_file = "";
+            try {
+                CommandLine cmd = parser.parse(options, args);
+                maze_file = cmd.getOptionValue("-i");
+
+            } catch (ParseException pe) {
+                logger.error("An error has occurred");
+            }
+            
+            //Read the maze file
+            logger.info("**** Reading the maze from file " + maze_file);
+            BufferedReader reader = new BufferedReader(new FileReader(maze_file));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -26,6 +44,7 @@ public class Main {
                 }
                 logger.info(System.lineSeparator());
             }
+            reader.close();
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
