60c3b0ebaab3153b5dcdcdea120e4987ca147b50
JakieBoi77 <jakef7@icloud.com>
2024-01-17 16:55:49 -0500

    Fixed logger

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -11,26 +11,26 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        logger("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
         try {
-            logger("**** Reading the maze from file " + args[0]);
+            logger.info("**** Reading the maze from file " + args[0]);
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
                     if (line.charAt(idx) == '#') {
-                        logger("WALL ");
+                        logger.info("WALL ");
                     } else if (line.charAt(idx) == ' ') {
-                        logger("PASS ");
+                        logger.info("PASS ");
                     }
                 }
-                logger(System.lineSeparator());
+                logger.info(System.lineSeparator());
             }
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
-        logger("**** Computing path");
-        logger("PATH NOT COMPUTED");
-        logger("** End of MazeRunner");
+        logger.info("**** Computing path");
+        logger.info("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
 }
