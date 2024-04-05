00aafff15e9652802d0e9a93958644c9f064b68e
JakieBoi77 <jakef7@icloud.com>
2024-04-05 00:44:26 -0400

    Modified pom to create the testing dependancy. Created a test class.

---
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/StandardMazeTest.java
@@ -0,0 +1,17 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class StandardMazeTest {
+
+    @BeforeAll
+    void setUp() {
+
+    }
+    
+    @Test
+    void testIsWall() {
+
+    }
+}
