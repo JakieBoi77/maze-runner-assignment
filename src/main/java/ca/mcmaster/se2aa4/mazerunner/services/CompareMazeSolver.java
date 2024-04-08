a73069f52ad4a7a36eda1e3cd036f29456d34567
JakieBoi77 <jakef7@icloud.com>
2024-04-08 16:45:51 -0400

    Cleaned up the benchmarking code.

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/services/CompareMazeSolver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/services/CompareMazeSolver.java
@@ -4,10 +4,9 @@ import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import ca.mcmaster.se2aa4.mazerunner.maze.Maze;
 import ca.mcmaster.se2aa4.mazerunner.maze.MazeFactory;
-import ca.mcmaster.se2aa4.mazerunner.maze.graph.GraphMaze;
 import ca.mcmaster.se2aa4.mazerunner.maze.graph.GraphMazeFactory;
-import ca.mcmaster.se2aa4.mazerunner.maze.standard.StandardMaze;
 import ca.mcmaster.se2aa4.mazerunner.maze.standard.StandardMazeFactory;
 import ca.mcmaster.se2aa4.mazerunner.util.Path;
 
@@ -18,102 +17,34 @@ public class CompareMazeSolver implements MazeService {
     private String mazeFile;
     private String method;
     private String baselineMethod;
-    
+    private String response;
+
     public CompareMazeSolver(String mazeFile, String method, String baselineMethod) {
         this.mazeFile = mazeFile;
         this.method = method;
         this.baselineMethod = baselineMethod;
+        this.response = "";
     }
 
     @Override
     public String run() {
-
-        MazeService service;
-        MazeService baselineService;
-
-        String response = "";
-
-        long startTime, endTime;
-
         try {
-
             logger.info("Calculating time to load maze.");
-
-            switch (method) {
-                case "righthand":
-                    MazeFactory<StandardMaze> standardMazeFactory = new StandardMazeFactory();
-                    StandardMaze standardMaze;
-                    
-                    // Calculate time for loading the maze
-                    startTime = System.nanoTime();
-                    standardMaze = standardMazeFactory.build(mazeFile);
-                    endTime = System.nanoTime();
-                    response += String.format("Time spent loading the maze for %s: %.2f milliseconds.\n", method, (endTime - startTime) / 1e6);
-
-                    service = new RightHandSolver(standardMaze);
-                    break;
-
-                case "bfs":
-                    MazeFactory<GraphMaze> graphMazeFactory = new GraphMazeFactory();
-                    GraphMaze graphMaze;
-
-                    // Calculate time for loading the maze
-                    startTime = System.nanoTime();
-                    graphMaze = graphMazeFactory.build(mazeFile);
-                    endTime = System.nanoTime();
-                    response += String.format("Time spent loading the maze for %s: %.2f milliseconds.\n", method, (endTime - startTime) / 1e6);
-
-                    service = new BreadthFirstSearchSolver(graphMaze);
-                    break;
-                default:
-                    throw new ParseException("Maze solving method '" + method + "' not supported.");
-            }
-
-            switch (baselineMethod) {
-                case "righthand":
-                    MazeFactory<StandardMaze> standardMazeFactory = new StandardMazeFactory();
-                    StandardMaze standardMaze;
-
-                    // Calculate time for loading the maze
-                    startTime = System.nanoTime();
-                    standardMaze = standardMazeFactory.build(mazeFile);
-                    endTime = System.nanoTime();
-                    response += String.format("Time spent loading the maze for %s: %.2f milliseconds.\n", baselineMethod, (endTime - startTime) / 1e6);
-
-                    baselineService = new RightHandSolver(standardMaze);
-                    break;
-
-                case "bfs":
-                    MazeFactory<GraphMaze> graphMazeFactory = new GraphMazeFactory();
-                    GraphMaze graphMaze;
-
-                    // Calculate time for loading the maze
-                    startTime = System.nanoTime();
-                    graphMaze = graphMazeFactory.build(mazeFile);
-                    endTime = System.nanoTime();
-                    response += String.format("Time spent loading the maze for %s: %.2f milliseconds.\n", baselineMethod, (endTime - startTime) / 1e6);
-
-                    baselineService = new BreadthFirstSearchSolver(graphMaze);
-                    break;
-                default:
-                    throw new ParseException("Maze solving method '" + baselineMethod + "' not supported.");
-            }
+            MazeService service = initializeMazeService(method);
+            MazeService baselineService = initializeMazeService(baselineMethod);
 
             logger.info("Calculating time to explore the maze.");
-
-            //Calculate the time spent for each method
-            startTime = System.nanoTime();
+            long startTime = System.nanoTime();
             String stringPath = service.run();
-            endTime = System.nanoTime();
-            response += String.format("Time spent exploring the maze for %s: %.2f milliseconds.\n", method, (endTime - startTime) / 1e6);
+            long endTime = System.nanoTime();
+            this.response += String.format("Time spent exploring the maze for %s: %.2f milliseconds.\n", method, (endTime - startTime) / 1e6);
+
             startTime = System.nanoTime();
             String stringBaselinePath = baselineService.run();
             endTime = System.nanoTime();
-            response += String.format("Time spent exploring the maze for %s: %.2f milliseconds.\n", baselineMethod, (endTime - startTime) / 1e6);
+            this.response += String.format("Time spent exploring the maze for %s: %.2f milliseconds.\n", baselineMethod, (endTime - startTime) / 1e6);
 
             logger.info("Calculating the speedup.");
-
-            //Calculate the speedup
             Path path = new Path(stringPath);
             Path baselinePath = new Path(stringBaselinePath);
             path.unfactorize();
@@ -121,12 +52,46 @@ public class CompareMazeSolver implements MazeService {
             int pathLength = path.getString().length();
             int baselinePathLength = baselinePath.getString().length();
             float speedup = (float) baselinePathLength / pathLength;
-            response += String.format("Speedup = %.2f\n", speedup);
+            this.response += String.format("Speedup = %.2f", speedup);
 
         } catch (ParseException pe) {
-            logger.error("******** Testing failed.  Reason: " + pe.getMessage());
+            logger.error("******** Testing failed. Reason: " + pe.getMessage());
         }
 
         return response;
+    }
+
+    private MazeService initializeMazeService(String method) throws ParseException {
+        MazeService service;
+        MazeFactory<?> mazeFactory;
+
+        switch (method) {
+            case "righthand":
+                mazeFactory = new StandardMazeFactory();
+                break;
+            case "bfs":
+                mazeFactory = new GraphMazeFactory();
+                break;
+            default:
+                throw new ParseException("Maze solving method '" + method + "' not supported.");
+        }
+
+        long startTime = System.nanoTime();
+        Maze maze = mazeFactory.build(mazeFile);
+        long endTime = System.nanoTime();
+        this.response += String.format("Time spent loading the maze for %s: %.2f milliseconds.\n", method, (endTime - startTime) / 1e6);
+
+        switch (method) {
+            case "righthand":
+                service = new RightHandSolver(maze);
+                break;
+            case "bfs":
+                service = new BreadthFirstSearchSolver(maze);
+                break;
+            default:
+                throw new ParseException("Maze solving method '" + method + "' not supported.");
+        }
+
+        return service;
     }
 }
